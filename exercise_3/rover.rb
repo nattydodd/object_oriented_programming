04cf186fd32137bd300473331fbb68999c786c29
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-11 17:32:15 -0400

    initialize

---
+++ exercise_3/rover.rb
@@ -0,0 +1,19 @@
+class Rover
+  def initialize(location_x, location_y, direction)
+    @location_x = location
+    @location_y = location
+    @direction = direction
+  end
+
+  def read_instruction
+  end
+
+
+  def turn
+  end
+
+  def move
+  end
+
+
+end
