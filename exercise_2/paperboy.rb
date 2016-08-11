c246d3a1ff9982f9c2b210a377650e4f06ccd160
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-11 15:55:46 -0400

    deliver method

---
+++ exercise_2/paperboy.rb
@@ -0,0 +1,35 @@
+class Paperboy
+
+  def initialize(name, side)
+    @name = name
+    @side = side
+    @experience = 0
+    @earnings = 0
+  end
+
+  def deliver
+    if @side = "even"
+    @experience = (@experience + (end_address - start_address)/2)
+    if @side = "odd"
+    @experience = (@experience + (end_address - start_address/2) -1
+    end
+  end
+
+  def quota
+    (@experience / 2) + 50
+  end
+
+  
+
+
+
+
+
+
+
+
+
+
+end
+
+jim = Paperboy.new("Jim", "even")
