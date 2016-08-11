c615fef914d3c89788a89807e0bf9115268cb09f
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-11 17:56:49 -0400

    defining the turn method

--- exercise_3/rover.rb
+++ exercise_3/rover.rb
@@ -9,11 +9,49 @@ class Rover
   end
 
 
-  def turn
+  def turn(rotation)
+    case rotation
+      when "L"
+        if @direction == "N"
+          @direction = "W"
+        elsif @direction == "E"
+          @direction = "N"
+        elsif @direction == "S"
+          @direction = "E"
+        elsif @direction == "W"
+          @direction = "S"
+        else
+          puts "does not exist"
+      end
+      when "R"
+        if @direction == "N"
+          @direction = "E"
+        elsif @direction == "E"
+          @direction = "S"
+        elsif @direction == "S"
+          @direction = "W"
+        elsif @direction == "W"
+          @direction = "N"
+        else
+          puts "does not exist"
+      end
+    end
   end
 
   def move
+    if @direction == "N"
+      @location_y += 1
+    elsif @direction == "E"
+      @location_x += 1
+    elsif @direction == "W"
+      @location_x -= 1
+    elsif @direction == "S"
+      @location_y -= 1
+    else
+      puts "doesn't exist"
+    end
   end
+
 
 
 end
