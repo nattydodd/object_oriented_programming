0953df2e60d72dcc4850698303c6e9f20bfb1fa7
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-11 16:49:02 -0400

    earnings defined

--- exercise_2/paperboy.rb
+++ exercise_2/paperboy.rb
@@ -7,19 +7,39 @@ class Paperboy
     @earnings = 0
   end
 
-  def deliver
-    if @side = "even"
-    @experience = (@experience + (end_address - start_address)/2)
-    if @side = "odd"
-    @experience = (@experience + (end_address - start_address/2) -1
+  def deliver(end_address, start_address)
+    papers_delivered = 0
+    if side == "odd"
+      start_address..end_address.each do |address|
+        if address % 2 != 0
+          papers_delivered += 1
+        end
+      end
+    if side == "even"
+      start_address..end_address.each do |address|
+        if address % 2 == 0
+          papers_delivered += 1
+        end
+      end
+    expected_quota = quota
+    if (papers_delivered - expected_quota) == 0
+      @earnings = paper_delivered * 0.25
+    elsif (papers_delivered - expected_quota) > 0
+      @earnings = (papers_delivered - expected_quota) * 0.50 + (expected_quota * 0.25)
+    else
+      @earnings = (papers_delivered * 0.25) - 2
     end
-  end
+    return @earnings
+   end
+
 
   def quota
     (@experience / 2) + 50
   end
 
-  
+
+
+
 
 
 
